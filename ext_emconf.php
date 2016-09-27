1a34a3ba1e2d4105de0c2a8ee9bd6a2df129c9a5
Maik Kempe <mkempe@bitaculous.com>
2016-09-27 12:41:51 +0200

    [➠] Updated dependencies.

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,8 @@ $EM_CONF[$_EXTKEY] = array(
   'clearCacheOnLoad' => true,
   'constraints' => array(
     'depends' => array(
-      'typo3' => '7.6.0-8.1.99'
+      'typo3' => '7.6.0-8.1.99',
+      't3v_core' => ''
     ),
     'conflicts' => array(),
     'suggests'  => array()
