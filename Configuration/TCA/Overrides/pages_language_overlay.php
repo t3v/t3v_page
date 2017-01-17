74e0ca3b75be1cd32d3432ab5c06c3f24b519ac9
Maik Kempe <mkempe@bitaculous.com>
2017-01-17 18:50:02 +0100

    [➠] Use default array syntax (`pages.php`, `pages_language_overlay.php`).

--- Configuration/TCA/Overrides/pages_language_overlay.php
+++ Configuration/TCA/Overrides/pages_language_overlay.php
@@ -2,55 +2,55 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 call_user_func(function() {
-  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages_language_overlay', array(
-    'tx_t3vpage_claim' => array(
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages_language_overlay', [
+    'tx_t3vpage_claim' => [
       'label' => 'LLL:EXT:t3v_page/Resources/Private/Language/locallang_tca.xlf:pages_language_overlay.tx_t3vpage_claim.label',
-      'config' => array(
+      'config' => [
         'type' => 'input',
         'size' => '40',
         'max' => 255,
         'eval' => 'trim'
-      ),
+      ],
       'l10n_mode' => 'mergeIfNotBlank',
       'exclude' => true
-    ),
+    ],
 
-    'tx_t3vpage_summary' => array(
+    'tx_t3vpage_summary' => [
       'label' => 'LLL:EXT:t3v_page/Resources/Private/Language/locallang_tca.xlf:pages_language_overlay.tx_t3vpage_summary.label',
-      'config' => array(
+      'config' => [
         'type' => 'text',
         'cols' => 40,
         'rows' => 3,
         'eval' => 'trim'
-      ),
+      ],
       'l10n_mode' => 'mergeIfNotBlank',
       'exclude' => true
-    ),
+    ],
 
-    'tx_t3vpage_thumbnail' => array(
+    'tx_t3vpage_thumbnail' => [
       'label' => 'LLL:EXT:t3v_page/Resources/Private/Language/locallang_tca.xlf:pages_language_overlay.tx_t3vpage_thumbnail.label',
       'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
         'image',
-        array(
+        [
           'maxitems' => 1,
-          'foreign_types' => array(
-            \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => array(
+          'foreign_types' => [
+            \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
               'showitem' => '--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette,--palette--;;imageoverlayPalette,--palette--;;filePalette'
-            )
-          )
-        ),
+            ]
+          ]
+        ],
         $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
       ),
       'l10n_mode' => 'mergeIfNotBlank',
       'exclude' => true
-    )
-  ));
+    ]
+  ]);
 
   $GLOBALS['TCA']['pages_language_overlay']['ctrl']['requestUpdate'] .= ',tx_t3vpage_exclude';
 
   \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('pages_language_overlay', '--palette--;LLL:EXT:t3v_page/Resources/Private/Language/locallang_tca.xlf:pages_language_overlay.palette.title;tx_t3vpage', '1', 'after:description');
 
-  $GLOBALS['TCA']['pages_language_overlay']['palettes']['tx_t3vpage'] = array(
+  $GLOBALS['TCA']['pages_language_overlay']['palettes']['tx_t3vpage'] = [
     'showitem' => 'tx_t3vpage_claim,--linebreak--,tx_t3vpage_summary,--linebreak--,tx_t3vpage_thumbnail'
-  );
+  ];
 });
