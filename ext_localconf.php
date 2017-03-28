bef5e9d88b7dbd3a4556515290e6e5b5041d2d9e
Maik Kempe <mkempe@bitaculous.com>
2017-03-28 15:13:57 +0200

    [✓] Fixed `ext_localconf.php`.

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,6 @@ defined('TYPO3_MODE') or die('Access denied.');
 call_user_func(function ($extkey) {
   // === TCA Configuration ===
 
-  $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_t3vpage_claim,tx_t3vpage_summary,tx_t3vpage_thumbnail';
-  $GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= ',tx_t3vpage_claim,tx_t3vpage_summary,tx_t3vpage_thumbnail';
+  $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_t3vpage_claim,tx_t3vpage_summary,tx_t3vpage_outline,tx_t3vpage_thumbnail';
+  $GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= ',tx_t3vpage_claim,tx_t3vpage_summary,tx_t3vpage_outline';
 }, $_EXTKEY);
