d07eb83b8b7dcab5872c1a277be7bc15e3cea5fd
Maik Kempe <mkempe@bitaculous.com>
2018-03-08 14:30:02 +0100

    [♻] Cleaned up.

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,8 +3,9 @@ defined('TYPO3_MODE') or die();
 
 // === Variables ===
 
-$extensionKey = 't3v_page';
+$extensionKey   = 't3v_page';
+$extensionTitle = 'T3v Page';
 
 // === TypoScript ===
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'T3v Page');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', $extensionTitle);
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// === Variables ===
-
-$namespace          = 't3v';
-$extensionKey       = $_EXTKEY;
-$extensionSignature = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($namespace . '.' . $extensionKey);
-
 // === Frontend Configuration ===
 
 // Add `tx_t3vpage_claim`, `tx_t3vpage_summary`, `tx_t3vpage_outline` and `tx_t3vpage_thumbnail` field to the root line.
