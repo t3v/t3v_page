03220bb3d9b3b050b319e50fa4cf0c7f5caa8fdd
Maik Kempe <mkempe@bitaculous.com>
2020-09-15 16:37:47 +0200

    [➠] Updated wording.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'T3v Page',
-    'description' => 'The page extension of TYPO3Voila.',
+    'description' => 'The page extension of TYPO3 voilà.',
     'author' => 'Maik Kempe',
     'author_email' => 'mkempe@bitaculous.com',
     'author_company' => 'Bitaculous - It\'s all about the bits, baby!',
