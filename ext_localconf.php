c7a4ea3e0ff54301483d57ea5289311acbc577d0
Maik Kempe <mkempe@bitaculous.com>
2022-04-27 14:44:39 +0200

    [➦] Merged changes.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,14 +3,14 @@ defined('TYPO3_MODE') or die();
 
 // === Frontend ===
 
-// Add `tx_t3vpage_claim`, `tx_t3vpage_summary`, `tx_t3vpage_outline` and `tx_t3vpage_thumbnail` field to the root line.
+// Adds `tx_t3vpage_claim`, `tx_t3vpage_summary`, `tx_t3vpage_outline` and `tx_t3vpage_thumbnail` field to the root line:
 if (empty($GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'])) {
   $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= 'tx_t3vpage_claim,tx_t3vpage_summary,tx_t3vpage_outline,tx_t3vpage_thumbnail';
 } else {
   $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_t3vpage_claim,tx_t3vpage_summary,tx_t3vpage_outline,tx_t3vpage_thumbnail';
 }
 
-// Add `tx_t3vpage_claim`, `tx_t3vpage_summary` and `tx_t3vpage_outline` field to the page overlay fields.
+// Adds `tx_t3vpage_claim`, `tx_t3vpage_summary` and `tx_t3vpage_outline` field to the page overlay fields:
 if (empty($GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'])) {
   $GLOBALS['TYPO3_CONF_VARS']['FE']['pageOverlayFields'] .= 'tx_t3vpage_claim,tx_t3vpage_summary,tx_t3vpage_outline';
 } else {
